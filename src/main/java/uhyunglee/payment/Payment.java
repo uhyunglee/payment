54026868bcc354a84e408d1b4d6c081a6c48a792
uhyunglee <uhyunglee@naver.com>
2026-03-25 22:57:54 +0900

    feat: add Payment domin model

--- src/main/java/uhyunglee/payment/Payment.java
+++ src/main/java/uhyunglee/payment/Payment.java
@@ -1,4 +1,34 @@
 package uhyunglee.payment;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Payment {
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount; // for concise amount
+    private BigDecimal exchangeRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime  validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exchangeRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exchangeRate = exchangeRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exchangeRate=" + exchangeRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
 }
--- src/main/java/uhyunglee/payment/PaymentService.java
+++ src/main/java/uhyunglee/payment/PaymentService.java
@@ -1,13 +1,21 @@
 package uhyunglee.payment;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class PaymentService {
-    public Payment prepare(){
-        return new Payment();
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount){
+        // get excahngeRate
+
+        // calculate amount
+
+        // calculate valid time
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO, LocalDateTime.now());
     }
 
     public static void main(String[] args) {
         PaymentService paymentService = new PaymentService();
-        Payment payment = paymentService.prepare();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
 }
