d17da54355177f4db3a30f0b535e6e5cba67c1e0
uhyunglee <uhyunglee@naver.com>
2026-04-02 21:02:45 +0900

    refactor: convert PaymentService to abstract class

--- src/main/java/uhyunglee/payment/PaymentService.java
+++ src/main/java/uhyunglee/payment/PaymentService.java
@@ -1,17 +1,11 @@
 package uhyunglee.payment;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
-public class PaymentService {
+abstract public class PaymentService {
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
         BigDecimal exchangeRate = getExchangeRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exchangeRate);
@@ -20,21 +14,10 @@ public class PaymentService {
         return new Payment(orderId, currency, foreignCurrencyAmount, exchangeRate, convertedAmount, validUntil);
     }
 
-    private BigDecimal getExchangeRate(String currency) throws IOException {
-        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String response = br.lines().collect(Collectors.joining());
-        br.close();
-
-        ObjectMapper mapper = new ObjectMapper();
-        ExchangedRateData data = mapper.readValue(response, ExchangedRateData.class);
-        BigDecimal exchangeRate = data.rates().get("KRW");
-        return exchangeRate;
-    }
+    abstract BigDecimal getExchangeRate(String currency) throws IOException;
 
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService();
+        PaymentService paymentService = new WebApiExRatePaymentService();
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
---
+++ src/main/java/uhyunglee/payment/WebApiExRatePaymentService.java
@@ -0,0 +1,28 @@
+package uhyunglee.payment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class WebApiExRatePaymentService extends PaymentService{
+    @Override
+    BigDecimal getExchangeRate(String currency) throws IOException {
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExchangedRateData data = mapper.readValue(response, ExchangedRateData.class);
+        BigDecimal exchangeRate = data.rates().get("KRW");
+
+        return exchangeRate;
+    }
+}
