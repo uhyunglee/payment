2f68b222fbdbbd7a190f14ce5bfbe0822ce1327c
uhyunglee <uhyunglee@naver.com>
2026-04-05 01:47:32 +0900

    refactor: add ObjectFactory

---
+++ src/main/java/uhyunglee/payment/ObjectFactory.java
@@ -0,0 +1,11 @@
+package uhyunglee.payment;
+
+public class ObjectFactory {
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    public ExRateProvider exRateProvider(){
+        return new WebApiExRateProvider();
+    }
+}
